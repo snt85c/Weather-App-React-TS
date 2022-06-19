caf6889b546fc7be6554fb3c9daeda566344b048
snt85c <santi.cacciola@hotmail.it>
2022-06-19 18:25:51 +0100

    add comments to swipe test 2

--- src/MainComponents/SwipeTest.tsx
+++ src/MainComponents/SwipeTest.tsx
@@ -32,57 +32,69 @@ export function TestSwipeDiv() {
 }
 
 export function TestSwipeDiv2() {
+    /*
+    -deltaX is the difference between the start point and the current point on the x axis
+    -opacityLx keeps tracks of the opacity of the left element, same for opacityRx. 
+    -messageRx is the value of the text in the div, which will change when the component is swipe over a certain amount
+    -swipeColor is the color of the background when swiping. right turns the background red to indicate deletion when swipe ends, left makes it purple to indicate highlight. in case of right, it returns to default color, for left it will change to purple only after a certain amoutn of swiping, otherwise default color.  can be red or purple or undefined as default state.
+     -highlight keeps only track of the bg color when i swipe all the way to highlight the component can be purple or undefined
+    */
   const [deltaX, setDeltaX] = useState<number>(0);
   const [opacityLx, setOpacityLx] = useState<number>(0);
   const [opacityRx, setOpacityRx] = useState<number>(0);
   const [messageRx, setMessageRx] = useState<string>("highlight");
-  const [color, setColor] = useState<string>("");
-  const [bgColor, setBgColor] = useState<string>("");
+  const [swipeColor, setSwipeColor] = useState<"red" | "purple" | "" >("");
+  const [highlight, setHighlight] = useState<"purple" | "">("");
 
   const swipeTest = useSwipeable({
     onSwiping: (e) => {
+      //sets a limit to how much the component can be moved on screen on x axis, as it stops setting the state after a certain amount, keeping it at 200/-200 while swiping 
       if (Math.abs(e.deltaX) < 200) {
         setDeltaX(e.deltaX);
       }
       if (e.dir === "Right") {
+        //if i detect a right swipe, set opacity of the diw with the message to delta/100 (so that it changes gradually, since opacity is a value between 0 and 1) as well as setting the color to red (this is conditionally rendered in the style), otherwise set color to purple
         setOpacityLx(deltaX / 100);
-        setColor("red");
+        setSwipeColor("red");
       } else {
         setOpacityRx(Math.abs(deltaX) / 100);
-        setColor("purple");
+        setSwipeColor("purple");
       }
     },
     onSwiped: (e) => {
-        setDeltaX(0);
-        setOpacityLx(0);
-        setOpacityRx(0);
+      //when i detect that the swiping is finished, set everything to default value, so that the div comes back to his original position and opacity
+      setDeltaX(0);
+      setOpacityLx(0);
+      setOpacityRx(0);
       if (deltaX && deltaX < -150) {
-        bgColor === "" ? setBgColor("purple") : setBgColor("");
+        //if i detect a swipe left more than 150, set the bg color to purple/remove the color and change the message
+        highlight === "" ? setHighlight("purple") : setHighlight("");
         messageRx === "highlight"
           ? setMessageRx("remove highlight")
           : setMessageRx("highlight");
       }
+      //condition  "delta > 150" is not present, as it should be a delete action, which is not useful for this test 
     },
   });
 
   return (
     <>
-      <div {...swipeTest} className="flex">
-        <div
-          className="flex m-1 px-2 justify-between items-center text-center text-white bg-amber-600 border border-amber-500 h-10 mx-3 duration-300 w-full"
-          style={{
-            transform: `translateX(${deltaX}px)`,
-            backgroundColor:
-              deltaX && (deltaX >= 150 || deltaX <= -150) ? color : bgColor,
-          }}
-        >
-          <div style={{ opacity: opacityLx }}>delete</div>
-          <div className="absolute left-[50%] right-[50%] top-auto flex flex-col justify-center items-center">
-            {/* {Math.abs(deltaX).toFixed(0)} */}
-            <div className="text-[0.8rem] -mt-2 leading-none">swipe 2</div>
-          </div>
-          <div style={{ opacity: opacityRx }}>{messageRx}</div>
+      <div
+        {...swipeTest}
+        className="flex m-1 px-2 justify-between items-center text-center text-white bg-amber-600 border border-amber-500 h-10 mx-3 duration-300"
+        style={{
+          transform: `translateX(${deltaX}px)`,
+          backgroundColor:
+            //if the value of deltaX is below a certain amount, use the color of the swipe direction (red or purple), if i go over the limit, set the color to the bgColor(highlighted in purple when swiping left)
+            deltaX && (deltaX >= 150 || deltaX <= -150) ? swipeColor : highlight,
+        }}
+      >
+        {/* one div on the left with delete, one centered with absolute values so that it wont change position when the right message changes, and a left div with a custom message */}
+        <div style={{ opacity: opacityLx }}>delete</div>
+        <div className="absolute left-[50%] right-[50%] top-auto flex flex-col justify-center items-center">
+          <div className="text-[0.8rem] -mt-2 leading-none">swipe 2</div>
         </div>
+        <div style={{ opacity: opacityRx }}>{messageRx}</div>
       </div>
     </>
   );
