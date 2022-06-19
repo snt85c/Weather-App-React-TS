2d481b0367e0e71447ac64c85d70ef91deedf182
snt85c <santi.cacciola@hotmail.it>
2022-06-19 16:17:58 +0100

    set limit to swipe
    the swipe is now limited to 200, as it looks better if the user is not able to swipe
    the entire element. this allows for the message to stay in the center of the screen
    when swiping the component as well as sets some boundaries to how far the element will
    go

--- src/MainComponents/SwipeTest.tsx
+++ src/MainComponents/SwipeTest.tsx
@@ -35,30 +35,32 @@ export function TestSwipeDiv2() {
   const [deltaX, setDeltaX] = useState<number>(0);
   const [opacityLx, setOpacityLx] = useState<number>(0);
   const [opacityRx, setOpacityRx] = useState<number>(0);
+  const [messageRx, setMessageRx] = useState<string>("highlight");
   const [color, setColor] = useState<string>("");
   const [bgColor, setBgColor] = useState<string>("");
 
-
   const swipeTest = useSwipeable({
     onSwiping: (e) => {
-      setDeltaX(e.deltaX);
+      if (Math.abs(e.deltaX) < 200) {
+        setDeltaX(e.deltaX);
+      }
       if (e.dir === "Right") {
-        setOpacityLx(deltaX / 100);
+        setOpacityLx(deltaX);
         setColor("red");
       } else {
-        setOpacityRx(Math.abs(deltaX / 100));
+        setOpacityRx(Math.abs(deltaX));
         setColor("purple");
       }
     },
     onSwiped: (e) => {
-      if (deltaX && (deltaX < 150 || deltaX > -150)) {
         setDeltaX(0);
         setOpacityLx(0);
         setOpacityRx(0);
-
-      }
-       if(deltaX && deltaX < -150){
-           bgColor=== ""?setBgColor("purple"):setBgColor("")
+      if (deltaX && deltaX < -150) {
+        bgColor === "" ? setBgColor("purple") : setBgColor("");
+        messageRx === "highlight"
+          ? setMessageRx("remove highlight")
+          : setMessageRx("highlight");
       }
     },
   });
@@ -75,11 +77,11 @@ export function TestSwipeDiv2() {
           }}
         >
           <div style={{ opacity: opacityLx }}>delete</div>
-          <div className="flex flex-col justify-center items-center">
-            {Math.abs(deltaX).toFixed(0)}
+          <div className="absolute left-[50%] right-[50%] top-auto flex flex-col justify-center items-center">
+            {/* {Math.abs(deltaX).toFixed(0)} */}
             <div className="text-[0.8rem] -mt-2">swipe test 2</div>
           </div>
-          <div style={{ opacity: opacityRx }}>highlight</div>
+          <div style={{ opacity: opacityRx }}>{messageRx}</div>
         </div>
       </div>
     </>
